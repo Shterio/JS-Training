06abd962109cf3c8b395b3ca0646e2a84609e73f
Shterio <Shterio@users.noreply.github.com>
2019-03-01 12:41:35 +0100

    more slotion

--- traidCommisions.js
+++ traidCommisions.js
@@ -1,22 +1,46 @@
 function name(input) {
-    let city = input[0];
+    let city = (input[0].toLowerCase());
     let sale = Number(input[1]);
 
     //let isCity = city == 'Sofia' || city == 'Varna' || city == 'Plovdiv';
     let commision = null;
 
-    if (city == 'Sofia') {
-        if (sale < 0 || sale <= 500) {
+    if (city == 'sofia') {
+        if (sale >= 0 && sale <= 500) {
             commision = sale * 0.05;
-        } else if (sale > 500 || sale <= 1000) {
+        } else if (sale > 500 && sale <= 1000) {
             commision = sale * 0.07;
-        } else if (sale > 1000 || sale <= 10000) {
+        } else if (sale > 1000 && sale <= 10000) {
             commision = sale * 0.08;
         } else if (sale > 10000) {
             commision = sale * 0.12;
-        }
-        
-    } console.log(commision.toFixed(2));
+        } 
+         
+    } else if (city == 'varna') {
+        if (sale >= 0 && sale <= 500) {
+            commision = sale * 0.045;
+        } else if (sale > 500 && sale <= 1000) {
+            commision = sale * 0.075;
+        } else if (sale > 1000 && sale <= 10000) {
+            commision = sale * 0.1;
+        } else if (sale > 10000) {
+            commision = sale * 0.13;
+        } 
+    } else if (city == 'plovdiv') {
+        if (sale >= 0 && sale <= 500) {
+            commision = sale * 0.055;
+        } else if (sale > 500 && sale <= 1000) {
+            commision = sale * 0.08;
+        } else if (sale > 1000 && sale <= 10000) {
+            commision = sale * 0.12;
+        } else if (sale > 10000) {
+            commision = sale * 0.145;
+        } 
+    } else {
+        console.log("error")
+    }
+
+    console.log(`${commision.toFixed(2)}`);
     
 }
-name(['Sofia', '1500'])
+name(['Kaspichan', '-50'])
