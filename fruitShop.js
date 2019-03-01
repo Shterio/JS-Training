10c80e331fa2a0d5ff33151f54401683ae8bf9da
Shterio <Shterio@users.noreply.github.com>
2019-03-01 11:59:45 +0100

    change

--- fruitShop.js
+++ fruitShop.js
@@ -3,44 +3,44 @@ function name(input) {
     let day = (input[1]).toLowerCase();
     let price = Number(input[2]);
 
-    let dayWeek = day =='Monday' || 'Tuesday' || 'Wednesday' || 'Thursday' || 'Friday';
-    let dayWeekend = day == 'Saturday' || 'Sunday';
-    //let fruitSort = fruit == 'banana' || 'apple' || 'orange' || 'grapefruit' || 'kiwi' || 'pineapple' || 'grapes';
-    let sum = 0;
+    let dayWeek = day == 'monday' || day == 'tuesday' || day == 'wednesday' || day == 'thursday' || day =='friday';
+    let isOffDay = day == 'saturday' || day == 'sunday';
+    
+    let sum = null;
 
     if (dayWeek) {
         if (fruit == 'banana') {
             sum = price * 2.5
         } else if (fruit == 'grapes') {
-            sum = price * 2.5
+            sum = price * 3.85
         } else if (fruit == 'apple') {
-            sum = price * 2.5
+            sum = price * 1.2
         } else if (fruit == 'orange') {
-            sum = price * 2.5
+            sum = price * 0.85
         } else if (fruit == 'grapefruit') {
-            sum = price * 2.5
+            sum = price * 1.45
         } else if (fruit == 'kiwi') {
-            sum = price * 2.5
+            sum = price * 2.7
         } else if (fruit == 'pineapple') {
-            sum = price * 2.5
+            sum = price * 5.5
         }
-    } else if (dayWeekend) {
+    } else if (isOffDay) {
         if (fruit == 'banana') {
-            sum = price * 2.5
+            sum = price * 2.7
         } else if (fruit == 'grapes') {
-            sum = price * 2.5
+            sum = price * 4.2
         } else if (fruit == 'apple') {
-            sum = price * 2.5
+            sum = price * 1.25
         } else if (fruit == 'orange') {
-            sum = price * 2.5
+            sum = price * 0.9
         } else if (fruit == 'grapefruit') {
-            sum = price * 2.5
+            sum = price * 1.6
         } else if (fruit == 'kiwi') {
-            sum = price * 2.5
+            sum = price * 3
         } else if (fruit == 'pineapple') {
-            sum = price * 2.5
+            sum = price * 5.6
         }
-    } //#endregion
+    } 
 
     if (sum) {
         console.log(sum.toFixed(2));
@@ -50,3 +50,5 @@ function name(input) {
         
     }
 }
+
+name(['apple', 'Workday', '2'])
