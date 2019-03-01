09e7652ce5a78c26cb3da75e79acc1022ef794c1
Shterio <Shterio@users.noreply.github.com>
2019-03-01 13:56:38 +0100

    final solution

--- skiTrip.js
+++ skiTrip.js
@@ -1,6 +1,65 @@
 function skiTrip(input) {
-    let 
+    let day = Number(input[0]);
+    let kind = input[1];
+    let note = input[2];
+
+    let days = day - 1;
+    let sum = null;
+    let price = null;
+    let notes = null;
+
+    if (kind == 'room for one person') {
+        if (days < 10) {
+            price = days * 18
+            notes = sum + (sum * 0.25)
+
+        } else if (days >= 10 && days <= 15) {
+            price = days * 18
+
+        } else {
+            price = days * 18
+            notes = sum + (sum * 0.25)
+
+    
+        } 
+    } else if (kind == 'apartment') {
+        if (days < 10) {
+            price = days * 25
+            sum = price - (price * 0.30)
+
+        } else if (days >= 10 && days <= 15) {
+            price = days * 25
+            sum = price - (price * 0.35)
+            notes = sum + (sum * 0.25)
+
+        } else {
+            price = days * 25
+            sum = price - (price * 0.5)
+        }
+    } else {
+        if (days < 10 ) {
+            price = days * 35
+            sum = price - (price * 0.10)
+
+        } else if (days >= 10 && days <= 15) {
+            price = days * 35
+            sum = price - (price * 0.15)
+            
+        } else {
+            price = days * 35
+            sum = price - (price * 0.2)
+        }
+    }
+    
+    if (note == 'positive') {
+        sum = sum + (sum * 0.25)
+    } else {
+        sum = sum - (sum * 0.1)
+    }
+    
+  console.log(sum.toFixed(2))     
 }
+    
 
 
-skiTrip([''])
+skiTrip(['14', 'apartment', 'positive'])
