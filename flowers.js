f547d9ded6c0960d563f1e429d7a553686812c0a
Shterio <Shterio@users.noreply.github.com>
2019-03-02 10:21:35 +0100

    solution

--- flowers.js
+++ flowers.js
@@ -5,19 +5,44 @@ function name(input) {
     
     let discount = null;
     let sum = null;
+    let money = null;
 
     if (typeOfFlower == "Roses") {
-        
+        sum = numberOfFlowers * 5;
+        if (numberOfFlowers > 80) {
+            discount = sum * 0.9;
+        }
     } else if (typeOfFlower == "Dahlias") {
-        
+        sum = numberOfFlowers * 3.8;
+        if (numberOfFlowers > 90) {
+            discount = sum * 0.85;
+        }
     } else if (typeOfFlower == "Tulips") {
-        
+        sum = numberOfFlowers * 2.8;
+        if (numberOfFlowers > 80) {
+            discount = sum * 0.85;
+        }
     } else if (typeOfFlower == "Narcissus") {
+        sum = numberOfFlowers * 3;
+        if (numberOfFlowers < 120) {
+            discount = sum * 1.15;
+        }
+    } else {
+        sum = numberOfFlowers * 2.5;
+        if (numberOfFlowers < 80) {
+            discount = sum * 1.20;
+        }
+    }
+
+    if (sum > budget) {
+        money = sum - budget;
+        console.log(`Not enough money, you need ${money.toFixed(2)} leva more.`);
         
     } else {
-
+        money = budget - sum;
+        console.log(`Hey, you have a great garden with ${numberOfFlowers} ${typeOfFlower} and ${money.toFixed(2)} leva left.`);
     }
     
 }
 
-name(['22', 'Afternoon'])
+name(['Roses', 55, 250])
