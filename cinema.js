119f7a4e1de9e1141844c523aa3a6fed6c19c454
Shterio <Shterio@users.noreply.github.com>
2019-03-02 09:08:02 +0100

    new task

---
+++ cinema.js
@@ -0,0 +1,22 @@
+function name(input) {
+    let film = input[0];
+    let row = Number(input[1]);
+    let columm = Number(input[2]);
+
+    let filmPrice = null;
+
+    if (film == 'Premiere') {
+        filmPrice = row * columm * 12;
+    } else if (film == 'Normal') {
+        filmPrice = row * columm * 7.5;
+    } else {
+        filmPrice = row * columm * 5;
+    }
+    console.log(`${filmPrice.toFixed(2)} leva`);
+    
+}
+
+name(['Premiere',
+    10,
+    12
+    ])
