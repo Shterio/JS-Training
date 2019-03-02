14ec20716753b66faf919230959498ffce89817e
Shterio <Shterio@users.noreply.github.com>
2019-03-02 08:36:51 +0100

    begin

---
+++ pointOnRectangle.js
@@ -0,0 +1,8 @@
+function name(input) {
+    let x1 = Number(input[0]);
+    let y1 = Number(input[1]);
+    let x2 = Number(input[2]);
+    let y2 = Number(input[3]);
+    let x = Number(input[4]);
+    let y = Number(input[5]);
+}
