4cba2d3e60473642abffe8a1fcee5b7f8a89c6b1
Shterio <Shterio@users.noreply.github.com>
2019-03-02 09:53:41 +0100

    new task

---
+++ flowers.js
@@ -0,0 +1,23 @@
+function name(input) {
+    let typeOfFlower = input[0];
+    let numberOfFlowers = Number(input[1]);
+    let budget = Number(input[2]);
+    
+    let discount = null;
+    let sum = null;
+
+    if (typeOfFlower == "Roses") {
+        
+    } else if (typeOfFlower == "Dahlias") {
+        
+    } else if (typeOfFlower == "Tulips") {
+        
+    } else if (typeOfFlower == "Narcissus") {
+        
+    } else {
+
+    }
+    
+}
+
+name(['22', 'Afternoon'])
