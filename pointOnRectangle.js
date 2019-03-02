9da2b127f68b1c63bdc68290b5b0c29e30481100
Shterio <Shterio@users.noreply.github.com>
2019-03-02 08:44:42 +0100

    solution

--- pointOnRectangle.js
+++ pointOnRectangle.js
@@ -5,4 +5,20 @@ function name(input) {
     let y2 = Number(input[3]);
     let x = Number(input[4]);
     let y = Number(input[5]);
+
+    if ((x1 < x && x < x2) && (y1 < y && y < y2)) {
+        console.log('Inside / Outside');
+        
+    } else {
+        console.log('Border');
+        
+    }
 }
+
+name([2,
+    -3,
+    12,
+    3,
+    12,
+    -1
+    ])
