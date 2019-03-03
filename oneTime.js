47c30763cebcb8acad9fd07c6efac34e3680bdfc
Shterio <Shterio@users.noreply.github.com>
2019-03-03 14:19:18 +0100

    new task

---
+++ oneTime.js
@@ -0,0 +1,6 @@
+function name(input) {
+    let examTime = Number(input[0]);
+    let examMinutes = Number(input[1]);
+    let timeOfComing = Number(input[2]);
+    let minutesOfComing = Number(input[3]);
+}
