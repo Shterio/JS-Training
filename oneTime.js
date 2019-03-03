70185aaf6131e9a6a3310edf67eb193f657defff
Shterio <Shterio@users.noreply.github.com>
2019-03-03 15:07:06 +0100

    second

--- oneTime.js
+++ oneTime.js
@@ -3,4 +3,11 @@ function name(input) {
     let examMinutes = Number(input[1]);
     let timeOfComing = Number(input[2]);
     let minutesOfComing = Number(input[3]);
+
+    let exam = examTime * 60 + examMinutes;
+    let coming = timeOfComing * 60 + minutesOfComing;
+
+    if (exam) {
+        
+    }
 }
