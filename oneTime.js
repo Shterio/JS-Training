9018821281cc0e011561accbdfa8586adff920ee
Shterio <Shterio@users.noreply.github.com>
2019-03-03 15:26:17 +0100

    more

--- oneTime.js
+++ oneTime.js
@@ -7,7 +7,20 @@ function name(input) {
     let exam = examTime * 60 + examMinutes;
     let coming = timeOfComing * 60 + minutesOfComing;
 
-    if (exam) {
-        
+    let finalMinutes = Math.abs(exam - coming);
+    let hours = Math.floor(finalMinutes / 60);
+    let minutes = finalMinutes % 60;
+
+    if (coming > exam) {
+        console.log('Late');
+        if (hours == 0) {
+            console.log(`${minutes} minutes after the start`);
+            
+        } else if (hours > 0 && minutes < 10) {
+            console.log(`${hours}:0${minutes} hours after the start`);
+            
+        } else if (hours > 0 && minutes >=10) {
+            console.log(`${hours}:${minutes} hours after the start`);
+        }
     }
 }
