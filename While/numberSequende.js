cc91534dc6f8925e4b8ef09c5ca0173e6ef59c72
Shterio <Shterio@users.noreply.github.com>
2019-03-10 19:24:15 +0100

    '''

--- While/numberSequende.js
+++ While/numberSequende.js
@@ -1,8 +1,27 @@
 function name(input) {
     let n = Number(input.shift());
 
-    
-    
+    let max = n;
+    let min = n;
+
+    while (n != 'END') {
+        n = Number(n);
+        if (n > max) {
+            max = n;
+        }
+        if (n < min) {
+            min = n;
+        }
+        n = input.shift();
+    }
+    console.log(`Max number: ${max}`);
+    console.log(`Min number: ${min}`);
 }
 
-name([3,5.51,69.42,100])
+name([10,
+    20,
+    304,
+    0,
+    50,
+    'END'
+    ])
