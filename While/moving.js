1059a97771dfe71ca5752d8eb57f878361b0bf79
Shterio <Shterio@users.noreply.github.com>
2019-03-16 19:11:01 +0100

    n

--- While/moving.js
+++ While/moving.js
@@ -1,3 +1,36 @@
 function name(input) {
-    
+    let width = Number(input.shift());
+    let length = Number(input.shift());
+    let height = Number(input.shift());
+
+    let currentBoxesNumber = input.shift();
+
+    let volume = width * length * height;
+
+    let takenSpace = 0;
+
+    while (takenSpace <= volume) {
+        if (currentBoxesNumber == 'Done') {
+            break;
+        }
+        currentBoxesNumber = Number(currentBoxesNumber);
+        takenSpace += currentBoxesNumber;       
+        currentBoxesNumber = input.shift();
+    }
+    let diff = Math.abs(volume - takenSpace)
+    if (takenSpace > volume) {
+        console.log(`No more free space! You need ${diff} Cubic meters more.`);
+        
+    }else{
+        console.log(`${diff} Cubic meters left.`);
+        
+    }
+
 }
+name([10,
+    1,
+    2,
+    4,
+    6,
+    'Done'
+    ])
