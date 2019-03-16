a3809d4909b3b0540ea98d25a045adb1884dabd4
Shterio <Shterio@users.noreply.github.com>
2019-03-16 13:29:56 +0100

    n

--- While/walking.js
+++ While/walking.js
@@ -1,3 +1,20 @@
 function name(input) {
+    const goal = 10000;
+    let steps = Number(input.shift());
+
+    let sumSteps = 0;
+
+    while (sumSteps <= goal) {
+        sumSteps += steps;
+        steps = Number(input.shift());
+    }
+
+    console.log('Goal reached! Good job!');
     
 }
+
+name([1000,
+    1500,
+    2000,
+    6500
+    ])
