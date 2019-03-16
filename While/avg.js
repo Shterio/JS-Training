74ac2717fbb3f93792f154e12cf8884763b26559
Shterio <Shterio@users.noreply.github.com>
2019-03-16 15:35:18 +0100

    n

--- While/avg.js
+++ While/avg.js
@@ -2,33 +2,45 @@ function name(input) {
     let name = input.shift();
     let count = 1;
     let sum = 0;
+    let excluded = 0;
+    let isExcluded = false;
 
     while (count <= 12) {
         let note = Number(input.shift());
 
+        if (note < 4) {
+            excluded++;
+        }
         if (note >= 4) {
             count++;
             sum += note;
         }
+        if (excluded >= 2) {
+            isExcluded = true;
+            break;
+        }
 
     }
-    let avg = sum / 12;
+    if (isExcluded == false) {
+        let avg = sum / 12;
         console.log(`${name} graduated. Average grade: ${avg.toFixed(2)}`);
-    
+    } else {
+        console.log(`${name} has been excluded at ${count} grade`);
+        
+    }
+
+
 }
 
-name(['Pesho',
-    4,
-    5.5,
-    6,
-    5.43,
-    4.5,
-    6,
-    5.55,
-    5,
-    6,
-    6,
-    5.43,
-    5
-    
-    ])
+name(['Mimi',
+5,
+6,
+5,
+6,
+5,
+6,
+6,
+2,
+3
+
+])
